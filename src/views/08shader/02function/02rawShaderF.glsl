cea80eb12304a5a6299ed49c5e31a96cdb63a3c9
@wang-yueming1 <wangyueming@newlinker.net>
2023-04-24 17:08:07 +0800

    1

--- src/views/08shader/02function/02rawShaderF.glsl
+++ src/views/08shader/02function/02rawShaderF.glsl
@@ -1,23 +1,5 @@
-// 你需要告知gpu 浮点数需要多少精度的小数点位数 如果电脑配置低  那么就调低
 precision lowp float;
-//        2d采样类型  纹理图片
-uniform sampler2D uTexture;
-
 varying vec2 vUv;
-// 获取z轴高度信息 用于制作阴影感
-// 因为顶点着色器中定义的波浪高度是0.05 这里我们希望-0.05到0.05之间的值能变成0.0到1.0
-varying float vElevation;
 void main() {
-    // 根据uv取出对应的颜色 采样     纹理              uv坐标
-    vec4 textureColor = texture2D(uTexture, vUv);
-
-    float height = vElevation + 0.05 * 12.0;
-    // gl_FragColor = vec4(1.0 * height, 0.0, 0.0, 1.0);
-
-    // 让纹理采样后的颜色矩阵中rgb三个值都乘以高度 这样采样后的纹理也可以被处理为有阴影的感觉
-    // 为什么？ 如果你只乘了红色的值  那么图片纹理中 绿色和蓝色的部分就不会有阴影感  因为你没有处理 他的颜色不会发生改变 
-    // 上面的基础面只有红色乘了是因为他就是红色 蓝绿是0
-    textureColor.rgb *= height;
-
-    gl_FragColor = textureColor;
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- src/views/08shader/02function/02rawShaderV.glsl
+++ src/views/08shader/02function/02rawShaderV.glsl
@@ -1,41 +1,14 @@
-// 原始着色器材质 你需要自己传入这些属性
-// 你需要告知gpu 浮点数需要多少精度的小数点位数 如果电脑配置低  那么就调低
 precision lowp float;
-// highp -2^6 - 2^16
-// mediump -2^10 - 2^10
-// lowp -2^8 - 2^8
-
-// vec3 三维向量
-// mat4 四维矩阵
 attribute vec3 position;
 attribute vec2 uv;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-// 将uv传递给片元 命名方式建议 传递给片元的变量 前面加v 然后后面驼峰命名
 varying vec2 vUv;
-
-// 获取传入的全局变量
 uniform float uTime;
 
-varying float vElevation;
 void main() {
     vUv = uv;
-    // 如果你想偏移顶点的坐标
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    // modelPosition.x++;
-    // modelPosition.z++;
-    // z轴本来全都是0  加了一个x的顶点位置 那么z轴就变成斜的了
-    // modelPosition.z += modelPosition.x;
-
-    // 制作波浪的效果  
-    // 千万记住  浮点数不能和整数相乘
-    // 顶点太少效果不明显                           时间变量   密度     高度
-    modelPosition.z = sin((modelPosition.x + uTime) * 10.0) * 0.05;
-    modelPosition.z += sin((modelPosition.y + uTime) * 10.0) * 0.05;
-
-    // 如何做出底部暗顶部亮 有阴影的感觉？
-    // 将顶点的高度信息传递给片元 这里波浪是用z轴做的 所以高度是z
-    vElevation = modelPosition.z;
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
